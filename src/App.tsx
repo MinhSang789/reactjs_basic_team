3a082ed609325ae14a1d0c39993f5f2c837e0664
minhsang0123 <minhsangcv99@gmail.com>
2022-07-06 17:54:21 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css';
+import './App.scss';
 import Home from "./pages/Home/Home";
 
 function App() {
